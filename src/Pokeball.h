36bc805f82f18a1f78175ed1ecda0cb3fdc770d0
ianco <ian.collet@laposte.net>
2023-02-09 14:40:05 +0100

    Fix test 2

--- src/Pokeball.h
+++ src/Pokeball.h
@@ -2,4 +2,17 @@
 
 // A ball where a Pokemon sleeps.
 class Pokeball
-{};
+{
+public:
+   Pokeball()
+   : _is_empty { true }
+   {}
+   
+   bool empty() const
+   {
+      return _is_empty;
+   }
+   
+private:
+   bool _is_empty;
+};
